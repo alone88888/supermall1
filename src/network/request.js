807861d2eda3103bd947da6964f7dc865c4126fe
alone88888 <183346877@qq.com>
2021-07-13 10:43:11 +0800

    update baseUrl

--- src/network/request.js
+++ src/network/request.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-export function request(config) {
+export function request (config) {
   const instance = axios.create({
     // baseURL: 'http://123.207.32.32:8000',
-    baseURL: 'http://152.136.185.210:8000/api/w6',
+    baseURL: 'http://152.136.185.210:7878/api/m5',
     timeout: 5000
   });
 
